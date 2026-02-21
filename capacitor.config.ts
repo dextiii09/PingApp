a9333c7b36d2777706ab6522557abe147bfb9956
dextiii09 <dhruvaman09@gmail.com>
2026-02-21 19:36:47 +0530

    fix(android): allow navigation to firebase domains and add custom tabs for recaptcha fallback to avoid internal-error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'co.pixelarcade.pingapp',
   appName: 'pingapp',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    allowNavigation: [
+      '*.firebaseapp.com',
+      '*.firebaseio.com',
+      '*.googleapis.com',
+      '*.google.com'
+    ]
+  }
 };
 
 export default config;
